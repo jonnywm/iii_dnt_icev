c4f735da4416a57894964f6e900edecba4ec5608
Jonny Magalhães <jonnywm@gmail.com>
2024-03-07 09:18:49 -0300

    (add) Collection Postman

--- src/main/java/br/com/icev/dnt/iii/controller/PessoaController.java
+++ src/main/java/br/com/icev/dnt/iii/controller/PessoaController.java
@@ -8,8 +8,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
